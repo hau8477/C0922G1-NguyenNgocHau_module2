3b8f66f4dca22368433f643f4719cb0d9c5b42d8
hau8477 <hau8477@gmail.com>
2022-11-10 14:27:57 +0700

    first commit

--- src/ss5_access_modifier/exercise/student/StudentMain.java
+++ src/ss5_access_modifier/exercise/student/StudentMain.java
@@ -3,11 +3,11 @@ package ss5_access_modifier.exercise.student;
 public class StudentMain {
     public static void main(String[] args) {
         Student student1 = new Student();
-        Student student2 = new Student();
-        student2.setName("NgocHau");
-        student2.setClasses("C0922G1");
-
         System.out.println("Student default: " + student1);
-        System.out.println("Student 1: " + student2);
+        
+        student1.setName("NgocHau");
+        student1.setClasses("C0922G1");
+
+        System.out.println("Student 1: " + student1);
     }
 }
