6f23f21942b0feca86f8424eae94308516944f99
hau8477 <hau8477@gmail.com>
2022-11-18 12:44:49 +0700

    first commit

--- src/ss11_stack_and_queue/exercise/count_word_in_string/CountWordInString.java
+++ src/ss11_stack_and_queue/exercise/count_word_in_string/CountWordInString.java
@@ -6,23 +6,21 @@ public class CountWordInString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter string: ");
+        System.out.print("Enter string: ");
         String document = scanner.nextLine();
 
         String[] arrayDocument = document.split(" ");
         Map<String, Integer> numberWord = new HashMap<>();
 
         for (String item : arrayDocument) {
-            String key = "";
             int value = 0;
             for (String s : arrayDocument) {
                 if (Objects.equals(item, s)) {
-                    key = item;
                     value++;
                 }
             }
-           if (!Objects.equals(key, "")) {
-               numberWord.put(key, value);
+           if (!Objects.equals(item, "")) {
+               numberWord.put(item, value);
            }
         }
         System.out.println(numberWord);
