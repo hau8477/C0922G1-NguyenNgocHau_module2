d037a416a54464ddef969fc9b03884c381d2f4ac
hau8477 <hau8477@gmail.com>
2022-11-23 15:01:39 +0700

    ss14

--- src/ss14_sort_algorithm/exercise/insert_sort/InsertSort.java
+++ src/ss14_sort_algorithm/exercise/insert_sort/InsertSort.java
@@ -1,25 +1,6 @@
 package ss14_sort_algorithm.exercise.insert_sort;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class InsertSort {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a length array: ");
-        int lengthArrayInteger = Integer.parseInt(scanner.nextLine());
-        int[] arrayInteger = new int[lengthArrayInteger];
-
-        for (int i = 0; i < lengthArrayInteger; i++) {
-            System.out.print("Enter values element " + (i + 1) + ": ");
-            arrayInteger[i] = Integer.parseInt(scanner.nextLine());
-        }
-
-        System.out.println("Your array input: " + Arrays.toString(arrayInteger));
-        insertSort(arrayInteger);
-    }
-
     public static void insertSort(int[] array) {
         int x, pos;
 
@@ -32,7 +13,5 @@ public class InsertSort {
             }
             array[pos] = x;
         }
-
-        System.out.println("Array after sorted: " + Arrays.toString(array));
     }
 }
