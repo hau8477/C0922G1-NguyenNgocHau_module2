a14f03084d86843372ba61accfd356f30098c51d
hau8477 <hau8477@gmail.com>
2022-11-24 16:30:35 +0700

    ss15

--- src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/Triangle.java
+++ src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/Triangle.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) throws IllegalTriangleException {
         Scanner scanner = new Scanner(System.in);
-     do {
-         try {
-             System.out.println("Enter side 1: ");
-             double side1 = Double.parseDouble(scanner.nextLine());
-             System.out.println("Enter side 2: ");
-             double side2 = Double.parseDouble(scanner.nextLine());
-             System.out.println("Enter side 3: ");
-             double side3 = Double.parseDouble(scanner.nextLine());
-             checkTriangle(side1,side2,side3);
-             break;
-         } catch (IllegalTriangleException e) {
-             System.err.println(e.getMessage());
-         } catch (NumberFormatException e) {
-             System.err.println("Triangle edges must be numbers");
-         }
-     } while (true);
+        double side1;
+        double side2;
+        double side3;
+        do {
+            try {
+                System.out.print("Enter side 1: ");
+                side1 = Double.parseDouble(scanner.nextLine());
+                System.out.print("Enter side 2: ");
+                side2 = Double.parseDouble(scanner.nextLine());
+                System.out.print("Enter side 3: ");
+                side3 = Double.parseDouble(scanner.nextLine());
+                checkTriangle(side1, side2, side3);
+                break;
+            } catch (IllegalTriangleException | NumberFormatException e) {
+                System.err.print(e.getMessage());
+            }
+        } while (true);
     }
 
     public static void checkTriangle(double side1, double side2, double side3) throws IllegalTriangleException {
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1+side2) <= side3
-                || (side1+side3) <= side2 || (side2+side3) <= side1) {
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1 + side2) <= side3
+                || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
             throw new IllegalTriangleException();
+        } else {
+            System.out.println("Is a triangle!");
         }
     }
 }
