368b689ad1548202b500fc738dd3d3a76923cc84
hau8477 <hau8477@gmail.com>
2022-11-28 08:45:30 +0700

    ss16

--- src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/IllegalTriangleException.java
+++ src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/IllegalTriangleException.java
@@ -1,10 +1,15 @@
 package ss15_exceptions_and_debug.exercise.use_class_IllegalTriangleException;
 
 public class IllegalTriangleException extends Exception{
+
+    private String msg;
+
+    public IllegalTriangleException(String msg) {
+        this.msg = msg;
+    }
+
     @Override
     public String getMessage() {
-        String message = "Triangle edges must be positive numbers\n" +
-                "Sum of 2 triangle edges must be bigger than the last triangle edge\n";
-        return message;
+        return msg;
     }
 }
--- src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/Triangle.java
+++ src/ss15_exceptions_and_debug/exercise/use_class_IllegalTriangleException/Triangle.java
@@ -25,11 +25,13 @@ public class Triangle {
     }
 
     public static void checkTriangle(double side1, double side2, double side3) throws IllegalTriangleException {
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1 + side2) <= side3
-                || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
-            throw new IllegalTriangleException();
-        } else {
-            System.out.println("Is a triangle!");
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            throw new IllegalTriangleException("Triangle edges must be positive numbers\n ");
         }
+
+        if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+            throw new IllegalTriangleException("Sum of 2 triangle edges must be bigger than the last triangle edge");
+        }
+        System.out.println("Is a triangle!");
     }
 }
