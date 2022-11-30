4794088c5020cecd23388f93322d54cede407c98
hau8477 <hau8477@gmail.com>
2022-11-30 15:40:32 +0700

    ss19

--- src/ss19_string_and_regex/exercise/validate_name_class/NameClasses.java
+++ src/ss19_string_and_regex/exercise/validate_name_class/NameClasses.java
@@ -2,7 +2,7 @@ package ss19_string_and_regex.exercise.validate_name_class;
 import java.util.regex.Pattern;
 
 public class NameClasses {
-    private static final String NAME_CLASS_REGEX = "^[A|C|G]\\d{4}[G-M]$";
+    private static final String NAME_CLASS_REGEX = "^[A|C|G]\\d{4}[G-I|K-M]$";
 
     public boolean validateNameClasses(String regex) {
         return Pattern.matches(NAME_CLASS_REGEX, regex);
